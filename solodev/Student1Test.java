3597f5557a291e9970db99058a418f6e825005a3
Jxino <141395272+Jxino@users.noreply.github.com>
2025-05-08 13:30:55 +0000

    Day 7 of the 1 commit per day

---
+++ solodev/Student1Test.java
@@ -0,0 +1,29 @@
+package solodev;
+
+class Student1 {
+    public static int serialNum = 1000;
+    public int studentID;
+    public String studentName;
+    public int grade;
+    public String address;
+
+    public Student1() {
+        serialNum++;
+        studentID = serialNum;
+    }
+
+    public String getStudentName() {
+        return this.studentName;
+    }
+
+    public void setStudentName(String name) {
+        this.studentName = name;
+    }
+}
+
+public class Student1Test {
+    public static void main(String[] args) {
+        Student1 s1 = new Student1();
+        System.out.println(s1.studentID);
+    }
+}
