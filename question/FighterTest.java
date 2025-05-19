1f87ce45ae6903aea08108f114ae6347a69f35b1
Jxino <141395272+Jxino@users.noreply.github.com>
2025-05-19 14:09:07 +0000

    feat: use Singleton and for-loop for deep copy
    - Introduced Singleton pattern in Rosters to centralize roster1 and roster2 management
    - Used enhanced for-loop for improved readability during array copying
    - Updated FighterTest to demonstrate deep copy and roster printing methods

---
+++ question/FighterTest.java
@@ -0,0 +1,119 @@
+package question;
+
+class Fighter {
+    private String name;
+    private String nationality;
+    private String rank;
+
+    public Fighter(String name, String nationality, String rank) {
+        this.name = name;
+        this.nationality = nationality;
+        this.rank = rank;
+    }
+
+    public Fighter() {}
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getNationality() {
+        return this.nationality;
+    }
+
+    public String getRank() {
+        return this.rank;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s | %s | %s", this.name, this.nationality, this.rank);
+    }
+}
+
+class Rosters {
+    private static Rosters instance = new Rosters();
+    private Fighter[] roster1;
+    private Fighter[] roster2;
+
+    private Rosters() {}
+
+    static public Rosters getInstance() {
+        return instance;
+    }
+
+    public void setRoster(Fighter[] roster1, Fighter[] roster2) {
+        this.roster1 = roster1;
+        this.roster2 = roster2;
+    }
+
+    public Fighter[] getRoster1() {
+        return this.roster1;
+    }
+
+    public Fighter[] getRoster2() {
+        return this.roster2;
+    }
+
+    public void printRoster1() {
+        for (Fighter fighter : this.roster1) {
+            System.out.println(fighter.toString());
+        }
+        System.out.println("\n");
+    }
+
+    public void printRoster2() {
+        for (Fighter fighter : this.roster2) {
+            System.out.println(fighter.toString());
+        }
+        System.out.println("\n");
+    }
+}
+
+public class FighterTest {
+    public static void main(String[] args) {
+        Fighter[] roster1 = new Fighter[6];
+        Fighter[] roster2 = new Fighter[6];
+        Rosters rosters = Rosters.getInstance();
+
+        roster1[0] = new Fighter("Islam Makhachev", "Russia", "#c");
+        roster1[1] = new Fighter("Arman Tsarukyan", "Armenia", "#1");
+        roster1[2] = new Fighter("Charles Oliveira", "Brazil", "#2");
+        roster1[3] = new Fighter("Justin Gaethje", "USA", "#3");
+        roster1[4] = new Fighter("Max Holloway", "USA", "#4");
+        roster1[5] = new Fighter("Dustin Poirier", "Brazil", "#5");
+
+        for (int i = 0; i < roster2.length; i++) {
+            roster2[i] = new Fighter();
+        }
+
+        for (int i = 0; i < roster2.length; i++) {
+            roster2[i].setName(roster1[i].getName());
+            roster2[i].setNationality(roster1[i].getNationality());
+            roster2[i].setRank(roster1[i].getRank());
+        }
+
+        rosters.setRoster(roster1, roster2);
+
+        rosters.printRoster1();
+        rosters.printRoster2();
+
+        rosters.getRoster1()[0] = new Fighter("Cornor McGregor", "Ireland", "#c");
+        // roster1[0] = new Fighter("Cornor McGregor", "Ireland", "#c");
+
+        rosters.printRoster1();
+        rosters.printRoster2();        
+    }
+}
